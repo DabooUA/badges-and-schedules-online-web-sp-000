185002005a1034653f380cd50ccdfbb935ed9098
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-19 01:11:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,14 @@ def badge_maker(name)
 puts("Hello, my name is #{name}.")
 end
 
-def batch_badge_creator(attendees)
-  attendees.each do |name|
-  return badge_maker(name)
+def batch_badge_creator(speakers)
+  badge_message = []
+  speakers.each do |speaker|
+  message = badge_maker(speaker)
+  badge_message << message
+  end
+  badge_message
 end
 
-def assign_rooms
+def assign_rooms(attendees)
+  rooms = 0
