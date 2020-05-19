742ab928788b50bc3308741a66d4af1501c660b2
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-19 01:15:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,3 +21,15 @@ def assign_rooms(speakers)
     end
   room_message
 end
+
+def printer(speakers)
+
+  badge_messages = batch_badge_creator(speakers)
+  badge_messages.each do |message|
+    puts message
+  end
+  room_messages = assign_rooms(speakers)
+  room_messages.each do |message|
+    puts message
+  end
+end
